96842c120dde75f0d0ac619c5e4c1b4232f80b23
PoomSmart <thatchapon.unp@outlook.com>
2020-04-06 19:02:52 +0700

    Improved script

--- locate-usage.sh
+++ locate-usage.sh
@@ -8,4 +8,4 @@ fi
 
 LOCATION=$1
 
-grep "\", NULL" mapping.h | sed -e $'s/[\t",]//g' | sed -e $'s/NULL.*$//g' | while read -r line; do echo "$line"; grep -nri "$line" $LOCATION; echo ""; done
+grep "\", NULL" mapping.h | sed -e $'s/[\t",]//g' | sed -e $'s/NULL.*$//g' | while read -r line; do echo "$line"; grep -nris --exclude=\*.{png,jpg,h,plist,strings,car,xpc,ttc,ttf,otf,bin} "$line" $LOCATION; echo ""; done
