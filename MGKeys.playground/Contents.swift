7496bd9375c7bee64c269509e32b5bbcd70147c3
PoomSmart <thatchapon.unp@outlook.com>
2020-04-06 10:27:11 +0700

    Better

--- MGKeys.playground/Contents.swift
+++ MGKeys.playground/Contents.swift
@@ -1,14 +1,44 @@
 import UIKit
 import CommonCrypto
 
-func CalculateObfuscatedKey(key: String) -> String
-{
+func CalculateObfuscatedKey(key: String) -> String {
     let pre = ("MGCopyAnswer" + key as String)
     var hash = [UInt8](repeating: 0, count:Int(CC_MD5_DIGEST_LENGTH))
         CC_MD5(pre, UInt32(pre.count), &hash)
     let md5Data = NSData(bytes: hash, length: Int(CC_MD5_DIGEST_LENGTH))
     let obfuscatedKey = md5Data.base64EncodedString()
     return obfuscatedKey.substring(to: String.Index(encodedOffset: 22))
+}
+
+func Md5StringForObfuscatedKey(key: String) -> String? {
+    guard key.count != 0 else {
+        return nil
+    }
+    
+    let base64 = key + "=="
+    let data = Data(base64Encoded: base64)
+    guard data!.count == 16 else {
+        return nil
+    }
+    
+    let bytes = data!.withUnsafeBytes { (bytes: UnsafePointer<UInt8>) in
+        Array(UnsafeBufferPointer(start: bytes, count: data!.count / MemoryLayout<UInt8>.stride))
+    }
+    
+    return bytes.map( { String(format: "%02hhx", $0) } ).joined()
+}
+
+func obfuscatedToMD5(_ filename: String) -> Int {
+    errno = 0
+    let realPath = Bundle.main.path(forResource: filename, ofType: "txt")
+    if freopen(realPath, "r", stdin) == nil {
+        perror(realPath)
+        return 1
+    }
+    while let line = readLine() {
+        print(Md5StringForObfuscatedKey(key: line)!)
+    }
+    return 0
 }
 
 // 0D3qCmmfJ/i1mpmjOsV7EA - confirmed
@@ -19,3 +49,5 @@ CalculateObfuscatedKey(key: "DeviceSupportsSpokenMessages")
 
 // M+WPVivF4iTnm1CC8c6h+A (haptic) - guessed
 CalculateObfuscatedKey(key: "pre-warm-disabled")
+
+obfuscatedToMD5("obfuscated")
