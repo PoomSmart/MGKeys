bf71d67823b1cfefaf7cd928eb4399cf2017bfa2
PoomSmart <thatchapon.unp@outlook.com>
2021-05-09 15:51:02 +0700

    Updated for Swift 5

--- MGKeys.playground/Contents.swift
+++ MGKeys.playground/Contents.swift
@@ -7,22 +7,22 @@ func CalculateObfuscatedKey(key: String) -> String {
         CC_MD5(pre, UInt32(pre.count), &hash)
     let md5Data = NSData(bytes: hash, length: Int(CC_MD5_DIGEST_LENGTH))
     let obfuscatedKey = md5Data.base64EncodedString()
-    return obfuscatedKey.substring(to: String.Index(encodedOffset: 22))
+    return String(obfuscatedKey.prefix(22))
 }
 
-func Md5StringForObfuscatedKey(key: String) -> String? {
+func MD5StringForObfuscatedKey(key: String) -> String? {
     guard key.count != 0 else {
         return nil
     }
     
     let base64 = key + "=="
     let data = Data(base64Encoded: base64)
-    guard data!.count == 16 else {
+    guard data?.count == 16 else {
         return nil
     }
     
-    let bytes = data!.withUnsafeBytes { (bytes: UnsafePointer<UInt8>) in
-        Array(UnsafeBufferPointer(start: bytes, count: data!.count / MemoryLayout<UInt8>.stride))
+    let bytes = data!.withUnsafeBytes {
+        $0.bindMemory(to: UInt8.self)
     }
     
     return bytes.map( { String(format: "%02hhx", $0) } ).joined()
@@ -36,7 +36,7 @@ func obfuscatedToMD5(_ filename: String) -> Int {
         return 1
     }
     while let line = readLine() {
-        print(Md5StringForObfuscatedKey(key: line)!)
+        print(MD5StringForObfuscatedKey(key: line)!)
     }
     return 0
 }
