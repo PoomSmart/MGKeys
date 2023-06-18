0854f4a0269571a5e1c45c77f90c723073ce64fd
Thatchapon Unprasert <thatchapon.unp@outlook.com>
2023-06-18 20:15:42 +0700

    Recovered more

--- unknown_keys_with_desc.py
+++ unknown_keys_with_desc.py
@@ -3,14 +3,9 @@ unknown_with_desc = {
     "2tBx5IzZX4CpUVJ79LfsVg": "tribeca",
     "5Y72WwdS5NYHdc00gEZ/DQ": "8DHlxr5ECKhTSL3HmlZQGQ: aqc-a, extlom-mac-address",
     "8bCjks0zVuOcdivIhytNbQ": "DeviceSupportsLiDAR (mismatched)",
-    "J1QHVh74Nnd6Rqyiq71/yw": "avd AppleARMIODevice > decode-samples-per-second",
     "JWjvsupiqWYtIGwr8yGd8w": "PerfLevelCapabilityTwo4IOS (mismatched)",
-    "LUy+wqFiBmou5hwM5OWbmA": "AppleOceanComputeMCU > Carrier Slot",
-    "m7lDS+oP8q0pGg+CO7RvPg": "IODeviceTree:/chosen > 4JalTKSe5a9I+mb00ATvag",
     "Mi/ME+v1wZqkvXFU3xX3KA": "PerfLevelCapabilityOne4IOS (mismatched)",
-    "njUcH4bm+JBmvASakDdObg": "IODeviceTree:/arm-io/isp > /4LCTfMhvzuiK6b557ir5Q",
     "oOV1jhJbdV3AddkcCg0AEA": "MetalPerfLevelCapabilityOne4IOS (mismatched)",
-    "qgfS+6l1exvcMW5Tfplv2Q": "AppleOceanComputeMCU > Carrier ID",
     "TqtrpIdOXIIlhOraMmq/pQ": "PerfLevelCapabilityiPadM1 (mismatched)",
     "yF2IQrYS4yyREV4ZkbLysw": "AppleFillmoreDevice, local-mac-address-64"
 }
