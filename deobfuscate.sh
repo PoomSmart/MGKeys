1c370f7ecc89ea7dc0c9069281c1be13a5059b81
Thatchapon Unprasert <thatchapon.unp@outlook.com>
2023-07-02 15:32:18 +0700

    iOS 17b2 ARM

--- deobfuscate.sh
+++ deobfuscate.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-# specify path to libMobileGestalt.dylib
-strings -arch arm64 -n 22 $1 | grep -i '^[a-zA-Z0-9\+\/]\{22\}$' > temp-hashes.txt
+DYLIB=$1
+[[ -z "$DYLIB" ]] && DYLIB=libMobileGestalt.dylib
+ARCH=$2
+[[ -z "$ARCH" ]] && ARCH=arm64
+
+strings -arch $ARCH -n 22 $DYLIB | grep -i '^[a-zA-Z0-9\+\/]\{22\}$' > temp-hashes.txt
 grep -v -f false-positives.txt temp-hashes.txt | sort -f > hashes.txt
 rm -f temp-hashes.txt
 python3 gen-mapping.py
